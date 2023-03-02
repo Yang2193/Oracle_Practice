b17fa8e175876c68267188eb2df5aa6c90b85b9b
nocxel <wlrudnjtkddn@naver.com>
2023-03-03 02:11:06 +0900

    PTData - UI 추가  --전체 조회(MEMBERINFO 테이블사용)  --PT 기록 삭제
     PTDataDAO
     - 기능 구현
     -- PT 기록 삭제하면 PT_REMAIN 증가시켜주기
     -- 삭제 시 해당 PT_REMAIN 가져와서 PT_DATA 수정
      ex )   5 4 3 2 1  인데  4 삭제
         >>  5   4 3 2  로 업데이트 해줌
      대신, pt가 0이 되기 전에 추가 되는일이 생기면 약간 퇴색되는
      기능
      ex )
      5 4 3 5 4 3 2 1 에서  첫 번째 4 삭제
      5   4 5 5 4 3 2 가 되버림...
       (뒷부분은 맞게 들어가서 나쁘지 않을지도..)

--- src/com/kh/gym/dao/PTDataDAO.java
+++ src/com/kh/gym/dao/PTDataDAO.java
@@ -1,6 +1,7 @@
 package com.kh.gym.dao;
 
 import com.kh.gym.util.Common;
+import com.kh.gym.vo.MemberInfoVO;
 import com.kh.gym.vo.PTDataVO;
 
 import java.sql.*;
@@ -14,6 +15,38 @@ public class PTDataDAO {
     Statement stmt = null;
     PreparedStatement pstmt = null;
     ResultSet rSet = null;
+
+    // pt 받는 사람만 조회하는 메소드
+    public List<MemberInfoVO> ptMemberList(){
+        List<MemberInfoVO> list = new ArrayList<>();
+        try {
+            conn = Common.getConnection();
+            stmt = conn.createStatement();
+            String sql = "SELECT * FROM MEMBERINFO WHERE SUBSTR(PNAME, 1, 2) = 'PT' ORDER BY MEM_ID";
+            rSet = stmt.executeQuery(sql);
+
+            while (rSet.next()) {
+                int id = rSet.getInt("MEM_ID");
+                String mName = rSet.getString("MNAME");
+                String pName = rSet.getString("PNAME");
+                int ptRemain = rSet.getInt("PT_REMAIN");
+                Date dDate = rSet.getDate("DUE_DATE");
+                String gender = rSet.getString("GENDER");
+                String pNum = rSet.getString("PHONE_NUM");
+                String lNum = rSet.getString("LOCKER");
+                Date rDate = rSet.getDate("REG_DATE");
+
+                MemberInfoVO vo = new MemberInfoVO(id, mName, pName, ptRemain, dDate, gender, pNum, lNum, rDate);
+                list.add(vo);
+            }
+            Common.close(rSet); // 연결과 역순으로 해제
+            Common.close(stmt);
+            Common.close(conn);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
     public List<PTDataVO> getPTInfoList() {
         List<PTDataVO> list = new ArrayList<>();
         try {
@@ -21,7 +54,8 @@ public class PTDataDAO {
             StringBuilder sql = new StringBuilder();
             sql.append("SELECT PT.MEM_ID, M.MNAME, T.TRAINER_NAME, TO_CHAR( PT.PT_DATE, 'YYYY-MM-DD') AS PT_DATE, PT.PT_REMAIN ");
             sql.append("FROM PT_DATA PT, MEMBERINFO M, TRAINERS T ");
-            sql.append("WHERE PT.MEM_ID = M.MEM_ID AND T.TRAINER_ID = PT.TRAINER_ID");
+            sql.append("WHERE PT.MEM_ID = M.MEM_ID AND T.TRAINER_ID = PT.TRAINER_ID ");
+            sql.append("ORDER BY PT_DATE DESC");
             pstmt = conn.prepareStatement(sql.toString());
             listAddResultSet(list);
         } catch (Exception e) {
@@ -38,6 +72,7 @@ public class PTDataDAO {
             sql.append("SELECT PT.MEM_ID, M.MNAME, T.TRAINER_NAME, TO_CHAR( PT.PT_DATE, 'YYYY-MM-DD') AS PT_DATE, PT.PT_REMAIN ");
             sql.append("FROM PT_DATA PT, MEMBERINFO M, TRAINERS T ");
             sql.append("WHERE PT.MEM_ID = M.MEM_ID AND T.TRAINER_ID = PT.TRAINER_ID AND PT.MEM_ID = ?");
+            sql.append("ORDER BY PT_DATE DESC");
             pstmt = conn.prepareStatement(sql.toString());
             pstmt.setInt(1, memberID);
             listAddResultSet(list);
@@ -100,7 +135,6 @@ public class PTDataDAO {
         // DATE 는 JAVA.SQL.DATE를 사용하는 것도?
         try {
             conn = Common.getConnection();
-            conn.setAutoCommit(false);
             String sql = String.format("SELECT PT_REMAIN FROM MEMBERINFO WHERE MEM_ID = %d", memberID);
             stmt = conn.createStatement();
             rSet = stmt.executeQuery(sql);
@@ -117,12 +151,14 @@ public class PTDataDAO {
                 pstmt.setInt(4, memberID);
 
                 pstmt.executeUpdate();
+                pstmt.clearParameters(); // pstmt를 비워줍니다. 재사용 하기 위해서..
 
-//              MEMBERINFO 의 PT_REMAIN을 UPDATE 하는 sql2
+                // MEMBERINFO 의 PT_REMAIN을 UPDATE 하는 sql2
                 String sql2 = "UPDATE MEMBERINFO SET PT_REMAIN = PT_REMAIN - 1 WHERE MEM_ID = ?";
                 pstmt = conn.prepareStatement(sql2);
                 pstmt.setInt(1, memberID);
                 pstmt.executeUpdate();
+                System.out.println("PT 기록이 완료되었습니다.");
             } else {
                 System.out.println("남은 PT횟수가 없습니다.");
             }
@@ -136,4 +172,60 @@ public class PTDataDAO {
         Common.close(conn);
     }
 
+    // pt 기록 삭제: 회원번호 입력하면 PT_DATA 보여줌 rownum입력으로 행을 삭제, MEMBERINFO 테이블의 PT_REMAIN 값 1증가 update
+    public void PTDataDelete() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("회원 번호 입력 : ");
+        int memberID = sc.nextInt();
+        // DATE 는 JAVA.SQL.DATE를 사용하는 것도?
+        try {
+            List<PTDataVO> list = getPTInfoList(memberID);
+            PTDataView(list);
+            System.out.println("삭제하실 행의 번호를 선택해 주세요 : ");
+            int rowNo = sc.nextInt();
+            conn = Common.getConnection();
+
+            // 입력받은 rowNo에 해당하는 PT_REMAIN 값을 가져오는 쿼리문입니다.
+            String sql1 = String.format("SELECT PT_REMAIN FROM PT_DATA WHERE ROWID = " +
+                    "(SELECT rid FROM (SELECT ROWNUM rn, ROWID rid, E.* FROM " +
+                    "(SELECT * FROM PT_DATA WHERE MEM_ID = %d ORDER BY PT_DATE DESC) E ) " +
+                    "WHERE rn = %d)", memberID, rowNo);
+            stmt = conn.createStatement();
+            rSet = stmt.executeQuery(sql1);
+            int deletePtRemain = 0;
+            if (rSet.next()) {
+                deletePtRemain = rSet.getInt("PT_REMAIN");
+            } else {
+                System.out.println("해당 컬럼 없습니다.");
+                return;
+            }
+
+            // 입력받은 rowNo를 통해 PT_DATA의 해당하는 행을 삭제하는 쿼리문 입니다.
+            String sql2 = String.format("DELETE FROM PT_DATA WHERE ROWID = " +
+                    "(SELECT rid FROM " +
+                    "(SELECT ROWNUM rn, ROWID rid, E.* FROM " +
+                    "(SELECT * FROM PT_DATA WHERE MEM_ID = %d ORDER BY PT_DATE DESC) E )" +
+                    " WHERE rn = %d)", memberID, rowNo);
+            stmt = conn.createStatement();
+            stmt.executeUpdate(sql2);
+
+            // stmt 는 재사용이 가능하다
+            // MEMBERINFO 테이블의 PT_REMAIN을 1증가시키는 쿼리문
+            String sql3 = String.format("UPDATE MEMBERINFO SET PT_REMAIN = PT_REMAIN + 1 WHERE MEM_ID = %d", memberID);
+            stmt = conn.createStatement();
+            stmt.executeUpdate(sql3);
+            // PT_DATA 에서도 1씩 증가시켜야 할까?   5  4  3  2  1 인데 4를 삭제 하면 4보다 작은 값들을 다 1씩 증가 시켜줘야 함
+            String sql4 = String.format("UPDATE PT_DATA SET PT_REMAIN = PT_REMAIN + 1 WHERE MEM_ID = %d " +
+                    "AND PT_REMAIN < %d", memberID, deletePtRemain);
+            stmt = conn.createStatement();
+            stmt.executeUpdate(sql4);
+            System.out.println("PT 기록 삭제가 완료 되었습니다.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Common.close(rSet);
+        Common.close(stmt);
+        Common.close(pstmt);
+        Common.close(conn);
+    }
 }
