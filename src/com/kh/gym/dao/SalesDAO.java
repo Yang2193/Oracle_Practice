009ef49357737cd38ea36693fe6d201912c31ade
thkim1230 <thkim1347@gmail.com>
2023-03-02 23:06:10 +0900

    수정

--- src/com/kh/gym/dao/SalesDAO.java
+++ src/com/kh/gym/dao/SalesDAO.java
@@ -3,6 +3,7 @@ import com.kh.gym.util.Common;
 import com.kh.gym.vo.SalesVO;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 public class SalesDAO {
@@ -110,8 +111,7 @@ public class SalesDAO {
     }
 
     public void salesInsert() {
-        System.out.print("주문번호 : ");
-        int orNo = sc.nextInt();
+        System.out.println("등록할 매출 정보를 입력 하세요");
         System.out.print("회원번호 : ");
         int memNo = sc.nextInt();
         System.out.print("회원이름 : ");
@@ -123,14 +123,16 @@ public class SalesDAO {
         System.out.print("구매날짜 : ");
         String pDate = sc.next();
 
-        String sql = "INSERT INTO SALES_STATEMENT(ORDER_NO,MEM_ID,MNAME,PURCHASE,SALES,P_DATE) VALUES ("
-                + orNo + ", " + memNo + ", " + "'" + mName + "'" + ", " + "'" + purchase + "'" + ", "
+        String sql = "INSERT INTO SALES_STATEMENT(ORDER_NO,MEM_ID,MNAME,PURCHASE,SALES,P_DATE) VALUES (SEQ_ORDER_NO.NEXTVAL"
+                + ", " + memNo + ", " + "'" + mName + "'" + ", " + "'" + purchase + "'" + ", "
                 + sales + ", " + "'" + pDate + "'" + ")";
         try {
             conn = Common.getConnection();
             stmt = conn.createStatement();
             int ret = stmt.executeUpdate(sql);
-            System.out.println("Return : " + ret);
+            if (ret == 0) {
+                System.out.println("입력을 잘못 하였습니다.");
+            }else System.out.println("등록이 완료 되었습니다.");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -165,7 +167,7 @@ public class SalesDAO {
             int ret = stmt.executeUpdate(query);
             if (ret == 0) {
                 System.out.println("존재하지 않는 주문번호 입니다.");
-            }else System.out.println("Return : " + ret);
+            }else System.out.println("수정이 완료 되었습니다.");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -174,7 +176,7 @@ public class SalesDAO {
     }
 
     public void salesDelete() {
-        System.out.print("지울 주문번호를 입력 하세요 : ");
+        System.out.print("삭제할 주문번호를 입력 하세요 : ");
         int order_no = sc.nextInt();
 
         String query = "DELETE FROM SALES_STATEMENT WHERE ORDER_NO =" + order_no ;
@@ -184,7 +186,7 @@ public class SalesDAO {
             int ret = stmt.executeUpdate(query);
             if (ret == 0) {
                 System.out.println("존재하지 않는 주문번호 입니다.");
-            }else System.out.println("Return : " + ret);
+            }else System.out.println("삭제가 완료 되었습니다. : ");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -196,34 +198,37 @@ public class SalesDAO {
         List<SalesVO> list = new ArrayList<>();
         try {
             conn = Common.getConnection();
-            System.out.print("원하는 날을 입력 하세요 : ");
+            System.out.print("원하는 날짜를 입력 하세요 (YYYY/MM/DD): ");
             String wtDate = sc.nextLine();
             String sql = "SELECT * FROM SALES_STATEMENT WHERE TO_CHAR(P_DATE,'YYYY/MM/DD') = ? ORDER BY ORDER_NO";
             pStmt = conn.prepareStatement(sql);
             pStmt.setString(1,wtDate);
             rs = pStmt.executeQuery();
-            while (rs.next()) {
-                int no = rs.getInt("ORDER_NO");
-                int id = rs.getInt("MEM_ID");
-                String name = rs.getString("MNAME");
-                String purchase = rs.getString("PURCHASE");
-                int sales = rs.getInt("SALES");
-                String pDate = rs.getString("P_DATE");
+            if (rs.next()) {
+                do {
+                    int no = rs.getInt("ORDER_NO");
+                    int id = rs.getInt("MEM_ID");
+                    String name = rs.getString("MNAME");
+                    String purchase = rs.getString("PURCHASE");
+                    int sales = rs.getInt("SALES");
+                    String pDate = rs.getString("P_DATE");
 
-                SalesVO vo = new SalesVO();
-                vo.setOrder_No(no);
-                vo.setMem_ID(id);
-                vo.setMName(name);
-                vo.setPurchase(purchase);
-                vo.setSales(sales);
-                vo.setP_DateStr((pDate));
-                list.add(vo);
+                    SalesVO vo = new SalesVO();
+                    vo.setOrder_No(no);
+                    vo.setMem_ID(id);
+                    vo.setMName(name);
+                    vo.setPurchase(purchase);
+                    vo.setSales(sales);
+                    vo.setP_DateStr((pDate));
+                    list.add(vo);
+                } while (rs.next());
+            }else {
+                System.out.println("매출이 존재하지 않습니다.");
             }
             Common.close(rs);
             Common.close(pStmt);
             Common.close(conn);
         } catch (Exception e) {
-            e.printStackTrace();
             System.out.println("매출이 존재하지 않습니다.");
         }
         return list;
@@ -233,29 +238,32 @@ public class SalesDAO {
         List<SalesVO> list = new ArrayList<>();
         try {
             conn = Common.getConnection();
-            System.out.print("원하는 월을 입력 하세요 : ");
+            System.out.print("원하는 날짜를 입력 하세요 (YYYY/MM): ");
             String wtDate = sc.nextLine();
-
             String sql = "SELECT * FROM SALES_STATEMENT WHERE TO_CHAR(P_DATE,'YYYY/MM') = ? ORDER BY ORDER_NO";
             pStmt = conn.prepareStatement(sql);
             pStmt.setString(1,wtDate);
             rs = pStmt.executeQuery();
-        while (rs.next()) {
-                int no = rs.getInt("ORDER_NO");
-                int id = rs.getInt("MEM_ID");
-                String name = rs.getString("MNAME");
-                String purchase = rs.getString("PURCHASE");
-                int sales = rs.getInt("SALES");
-                String pDate = rs.getString("P_DATE");
+            if (rs.next()) { // 결과가 존재할 때
+                do {
+                    int no = rs.getInt("ORDER_NO");
+                    int id = rs.getInt("MEM_ID");
+                    String name = rs.getString("MNAME");
+                    String purchase = rs.getString("PURCHASE");
+                    int sales = rs.getInt("SALES");
+                    String pDate = rs.getString("P_DATE");
 
-                SalesVO vo = new SalesVO();
-                vo.setOrder_No(no);
-                vo.setMem_ID(id);
-                vo.setMName(name);
-                vo.setPurchase(purchase);
-                vo.setSales(sales);
-                vo.setP_DateStr((pDate));
-                list.add(vo);
+                    SalesVO vo = new SalesVO();
+                    vo.setOrder_No(no);
+                    vo.setMem_ID(id);
+                    vo.setMName(name);
+                    vo.setPurchase(purchase);
+                    vo.setSales(sales);
+                    vo.setP_DateStr(pDate);
+                    list.add(vo);
+                } while (rs.next());
+            } else { // 결과가 없을 때
+                System.out.println("매출이 존재하지 않습니다.");
             }
             Common.close(rs);
             Common.close(pStmt);
@@ -266,40 +274,43 @@ public class SalesDAO {
         return list;
     }
 
-    public List<SalesVO> somYearSalSel() {
+        public List<SalesVO> somYearSalSel() {
         List<SalesVO> list = new ArrayList<>();
 
         try {
-            conn = Common.getConnection();
-            System.out.print("원하는 년도를 입력 하세요 : ");
+            System.out.print("원하는 날짜를 입력 하세요 (YYYY): ");
             String wtDate = sc.nextLine();
             String sql = "SELECT * FROM SALES_STATEMENT WHERE TO_CHAR(P_DATE,'YYYY') = ? ORDER BY ORDER_NO";
-            pStmt = conn.prepareStatement(sql);
-            pStmt.setString(1,wtDate);
-            rs = pStmt.executeQuery();
-            while (rs.next()) {
-                int no = rs.getInt("ORDER_NO");
-                int id = rs.getInt("MEM_ID");
-                String name = rs.getString("MNAME");
-                String purchase = rs.getString("PURCHASE");
-                int sales = rs.getInt("SALES");
-                String pDate = rs.getString("P_DATE");
+                conn = Common.getConnection();
+                pStmt = conn.prepareStatement(sql);
+                pStmt.setString(1, wtDate);
+                rs = pStmt.executeQuery();
+                if (rs.next()) {
+                    do {
+                        int no = rs.getInt("ORDER_NO");
+                        int id = rs.getInt("MEM_ID");
+                        String name = rs.getString("MNAME");
+                        String purchase = rs.getString("PURCHASE");
+                        int sales = rs.getInt("SALES");
+                        String pDate = rs.getString("P_DATE");
 
-                SalesVO vo = new SalesVO();
-                vo.setOrder_No(no);
-                vo.setMem_ID(id);
-                vo.setMName(name);
-                vo.setPurchase(purchase);
-                vo.setSales(sales);
-                vo.setP_DateStr((pDate));
-                list.add(vo);
-            }
-            Common.close(rs);
-            Common.close(pStmt);
-            Common.close(conn);
+                        SalesVO vo = new SalesVO();
+                        vo.setOrder_No(no);
+                        vo.setMem_ID(id);
+                        vo.setMName(name);
+                        vo.setPurchase(purchase);
+                        vo.setSales(sales);
+                        vo.setP_DateStr((pDate));
+                        list.add(vo);
+                    } while (rs.next());
+                } else {
+                    System.out.println("매출이 존재하지 않습니다.");
+                }
+                Common.close(rs);
+                Common.close(pStmt);
+                Common.close(conn);
         } catch (Exception e) {
             System.out.println("매출이 존재하지 않습니다.");
-
         }
         return list;
     }
